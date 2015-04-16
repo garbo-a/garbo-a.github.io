392d539a1ce2383477c45c8f739e7948dfc3271e
Александр <garbovskij.a@gmail.com>
2015-04-16 10:04:25 +0300

    added Acrostia prjct

---
+++ acrostia-one-page-portfolio/scripts/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Александр on 14.04.2015.
+ */
+var slideshow=new TINY.slider.slide('slideshow',{
+    id:'slider',
+    auto:4,
+    resume:false,
+    vertical:false,
+    navid:'pagination',
+    activeclass:'current',
+    position:0,
+    rewind:false,
+    elastic:true,
+    left:'slideleft',
+    right:'slideright'
+});
