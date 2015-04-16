fb7f8410abf82e05752ac7bb97090b440a9bfd59
garbo-a <garbovskij.a@gmail.com>
2015-04-16 17:30:14 +0300

    wrapper stage#1 -  jquery wrapper ready (1st method without preActive CLASS)

---
+++ acrostia-one-page-portfolio/scripts/wrapper.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Александр on 14.04.2015.
+ */
+var main = function() {
+    $('.arrow-right').click(function() {
+        var currentSlide = $('.item.active');
+        var nextSlide = currentSlide.next();
+
+        if(nextSlide.length === 0) {
+            nextSlide = $('.item').first();
+        }
+
+        currentSlide.fadeOut(600).removeClass('active');
+        nextSlide.fadeIn(600).addClass('active');
+    })
+    $('.arrow-left').click(function() {
+        var currentSlide = $('.item.active');
+        var prevSlide = currentSlide.prev();
+
+        if(prevSlide.length === 0) {
+            prevSlide = $('.item').last();
+        }
+
+        currentSlide.fadeOut(600).removeClass('active');
+        prevSlide.fadeIn(600).addClass('active');
+    })
+}
+$(document).ready(main);
