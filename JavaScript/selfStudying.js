9fb14575fd07b672acc3384e6e7aa5107f75b366
Александр <garbovskij.a@gmail.com>
2015-05-05 19:13:11 +0300

    GO_iconia - optimisation of animation & @media (Sasha Sidko rev)

---
+++ JavaScript/selfStudying.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Александр on 04.05.2015.
+ */
+
+function checkAge(age) {
+    if (age > 18) {
+        return confirm('Welcome');
+    } else {
+        return confirm('Родители разрешили?');
+    }
+}
+var age = prompt('Specify your age:');
+checkAge(age);
+
+function checkAgeTwo(age2) {
+
+  (age2>18) ? confirm('Welcome') : confirm('Родители разрешили?');
+}
+var age2 = prompt('Specify your age:');
+checkAgeTwo(age2);
+
+function checkMin(a,b) {
+    if(a<b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+checkMin(2, 5);
+checkMin(3, -1);
+checkMin(1, 1);
+
+
+function pow(x,n) {
+    var i;
+
+    for (i = 1; i < n; i++) {
+        x = x * x;
+    }
+    alert(x);
+}
+
+pow(3, 2);
+pow(3, 3);
+pow(1, 100);
