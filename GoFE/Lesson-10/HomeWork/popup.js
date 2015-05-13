716a3fff3d278f5b46b4db9110c2b90780627e3a
garbo-a <garbovskij.a@gmail.com>
2015-05-13 15:43:54 +0300

    lesson 10 - homework revision 5 (incl jQuery)

--- GoFE/Lesson-10/HomeWork/popup.js
+++ GoFE/Lesson-10/HomeWork/popup.js
@@ -1,3 +1,11 @@
 /**
  * Created by Garbovskiy on 13.05.15.
  */
+
+var main = function() {
+    $('.portfolio__popup-zoom').click(function() {
+        $('.portfolio__popup').addClass('portfolio__popup_visible_yes');
+        $('.portfolio__overlay').addClass('portfolio__popup_visible_yes');
+    });
+};
+$(document).ready(main);
