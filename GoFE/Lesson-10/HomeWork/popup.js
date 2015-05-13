9bd17d8bb163da25db5146d0e440de4f97b4894f
garbo-a <garbovskij.a@gmail.com>
2015-05-13 16:00:29 +0300

    lesson 10 - homework revision 5 (incl jQuery show-hide)

--- GoFE/Lesson-10/HomeWork/popup.js
+++ GoFE/Lesson-10/HomeWork/popup.js
@@ -7,5 +7,8 @@ var main = function() {
         $('.portfolio__popup').addClass('portfolio__popup_visible_yes');
         $('.portfolio__overlay').addClass('portfolio__popup_visible_yes');
     });
-};
+    $('.portfolio__popup-close').click(function() {
+        $('.portfolio__popup').removeClass('portfolio__popup_visible_yes');
+        $('.portfolio__overlay').removeClass('portfolio__popup_visible_yes');
+    });};
 $(document).ready(main);
