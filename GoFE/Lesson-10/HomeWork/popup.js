a2f81faf7ef13c8f1ba5720426880b61f2eed0fe
garbo-a <garbovskij.a@gmail.com>
2015-05-13 17:57:40 +0300

    lesson 10 - homework revision 6 (working at .data)

--- GoFE/Lesson-10/HomeWork/popup.js
+++ GoFE/Lesson-10/HomeWork/popup.js
@@ -3,12 +3,26 @@
  */
 
 var main = function() {
-    $('.portfolio__popup-zoom').click(function() {
+   /* $('.portfolio__popup-zoom').click(function() {
         $('.portfolio__popup').addClass('portfolio__popup_visible_yes');
         $('.portfolio__overlay').addClass('portfolio__popup_visible_yes');
     });
     $('.portfolio__popup-close').click(function() {
         $('.portfolio__popup').removeClass('portfolio__popup_visible_yes');
         $('.portfolio__overlay').removeClass('portfolio__popup_visible_yes');
-    });};
+    });}; */
+
+    $('.portfolio__item').on("click",$('.portfolio__popup-zoom'),function() {
+        $('.portfolio__popup').addClass('portfolio__popup_visible_yes');
+        $('.portfolio__overlay').addClass('portfolio__popup_visible_yes').data();
+        /* вот тут не могу разобраться */
+    });
+
+    $('.portfolio__popup-close').click(function() {
+        $('.portfolio__popup').removeClass('portfolio__popup_visible_yes');
+        $('.portfolio__overlay').removeClass('portfolio__popup_visible_yes');
+    });
+};
+
 $(document).ready(main);
+
