8a5869db956cf4ff6a1da7244adb83afa338843f
garbo-a <garbovskij.a@gmail.com>
2015-05-14 11:37:58 +0300

    lesson 10 - homework revision 7 (jQuery popup ready)

--- GoFE/Lesson-10/HomeWork/popup.js
+++ GoFE/Lesson-10/HomeWork/popup.js
@@ -2,23 +2,14 @@
  * Created by Garbovskiy on 13.05.15.
  */
 
-var main = function() {
-   /* $('.portfolio__popup-zoom').click(function() {
-        $('.portfolio__popup').addClass('portfolio__popup_visible_yes');
+var main = function () {
+    $('.portfolio__item').on("click", $('.portfolio__popup-zoom'), function (event) {
+        var i = $(event.delegateTarget).data('image-popup');
+        $('.portfolio__popup').addClass('portfolio__popup_visible_yes').css("background", i);
         $('.portfolio__overlay').addClass('portfolio__popup_visible_yes');
     });
-    $('.portfolio__popup-close').click(function() {
-        $('.portfolio__popup').removeClass('portfolio__popup_visible_yes');
-        $('.portfolio__overlay').removeClass('portfolio__popup_visible_yes');
-    });}; */
 
-    $('.portfolio__item').on("click",$('.portfolio__popup-zoom'),function() {
-        $('.portfolio__popup').addClass('portfolio__popup_visible_yes');
-        $('.portfolio__overlay').addClass('portfolio__popup_visible_yes').data();
-        /* вот тут не могу разобраться */
-    });
-
-    $('.portfolio__popup-close').click(function() {
+    $('.portfolio__popup-close').click(function () {
         $('.portfolio__popup').removeClass('portfolio__popup_visible_yes');
         $('.portfolio__overlay').removeClass('portfolio__popup_visible_yes');
     });
