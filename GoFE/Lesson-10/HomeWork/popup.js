a9d2d6905e726192d544096347792043f5385a67
garbo-a <garbovskij.a@gmail.com>
2015-05-14 11:39:37 +0300

    lesson 10 - homework revision 7 (jQuery popup ready)

--- GoFE/Lesson-10/HomeWork/popup.js
+++ GoFE/Lesson-10/HomeWork/popup.js
@@ -3,6 +3,15 @@
  */
 
 var main = function () {
+    /* $('.portfolio__popup-zoom').click(function() {
+     $('.portfolio__popup').addClass('portfolio__popup_visible_yes');
+     $('.portfolio__overlay').addClass('portfolio__popup_visible_yes');
+     });
+     $('.portfolio__popup-close').click(function() {
+     $('.portfolio__popup').removeClass('portfolio__popup_visible_yes');
+     $('.portfolio__overlay').removeClass('portfolio__popup_visible_yes');
+     });}; этот код работает с оригинальным макетом домашнего задания*/
+
     $('.portfolio__item').on("click", $('.portfolio__popup-zoom'), function (event) {
         var i = $(event.delegateTarget).data('image-popup');
         $('.portfolio__popup').addClass('portfolio__popup_visible_yes').css("background", i);
