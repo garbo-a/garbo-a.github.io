965ffd527fc53f7a6b0de29042758557ca8715da
Garbovskiy <garbovskij.a@gmail.com>
2015-07-15 10:22:13 +0300

    efarmer test v1.4 (working on function play() )

--- efarmer__landing/script-page-scrolling.js
+++ efarmer__landing/script-page-scrolling.js
@@ -1,21 +1,3 @@
 /**
  * Created by Garbovskiy on 14.07.2015.
  */
-$(document).ready(function () {
-    //SCROLL TO ANCHOR TAG
-    var $root = $('html, body');
-
-    $('.goto').click(function () {
-        var $gotoid = $(this).prop('id');
-        var $justid = $gotoid.replace('goto', '');
-
-        if (typeof $("#" + $justid) != undefined) { // test if target exists
-            $root.animate({
-                scrollTop: $("#" + $justid).offset().top - $('#header').height()
-            }, 1000);
-        }
-        $('.goto_active_yes').removeClass('goto_active_yes');
-        $(this).addClass('goto_active_yes');
-        return false;
-    });
-});
