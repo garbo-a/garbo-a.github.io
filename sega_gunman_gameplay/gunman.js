c4d0ef3b2f6477b0660e9285ca206dc16a477d35
garbo-a <garbovskij.a@gmail.com>
2015-09-12 16:25:28 +0300

    working at gunman (gameplay ready) - needs sounds

--- sega_gunman_gameplay/gunman.js
+++ sega_gunman_gameplay/gunman.js
@@ -1,32 +1,37 @@
 function Gunman() {
     //Local variables
     var pathGunmanCounter = -100,
-        bcgGunmanCounter = 0,
-        gunmansArr = [{bgPosY: 0, timer: 1.00, reward: 100}, {bgPosY: -206, timer: 0.80, reward: 150}, {
-            bgPosY: -412,
-            timer: 0.70,
-            reward: 200
-        }, {
-            bgPosY: -618,
-            timer: 0.65, reward: 250
-        }, {bgPosY: -824, timer: 0.59, reward: 300}, {timer: 0.59, reward: 0}],
+        bgGunmanCounter = 0,
+        gunmansArr = [
+            {bgPosY: 0, timer: 1.00, reward: 100},
+            {bgPosY: -206, timer: 0.80, reward: 150},
+            {bgPosY: -412, timer: 0.70, reward: 200},
+            {bgPosY: -618, timer: 0.65, reward: 250},
+            {bgPosY: -824, timer: 0.59, reward: 300},
+            {timer: 0.59, reward: 0}
+        ],
+        MAX_PLAY = 5,
         globalCounter = 0,
         gunman = document.body.querySelector('.gunman'),
-        fire = document.body.querySelector('.fire'),
-        ready = document.querySelector('.ready'),
-        win = document.querySelector('.win'),
+        Bubbles = {
+            fire: document.body.querySelector('.bubble__fire'),
+            win: document.querySelector('.bubble__win'),
+            ready: document.querySelector('.bubble__ready')
+        },
+        gameOver = document.querySelector('.game__over'),
         winBg = document.querySelector('.win__bg'),
-        loose = document.querySelector('.loose'),
         playerTime = document.querySelector('.your__time span'),
         gunmanTime = document.querySelector('.gunman__time span'),
         reward = document.querySelector('.amount__reward span'),
-        audio_intro = document.querySelector('.intro'),
-        audio_death = document.querySelector('.death'),
-        audio_fire = document.querySelector('.fire'),
-        audio_shot = document.querySelector('.shot'),
-        audio_shot_fall = document.querySelector('.shot-fall'),
-        audio_wait = document.querySelector('.wait'),
-        audio_win = document.querySelector('.win'),
+        Audio = {
+            audioIntro: document.querySelector('.sound__intro'),
+            audioDeath: document.querySelector('.sound__death'),
+            audioFire: document.querySelector('.sound__fire'),
+            audioShot: document.querySelector('.sound__shot'),
+            audioShot_Fall: document.querySelector('.sound__shot-fall'),
+            audioWait: document.querySelector('.sound__wait'),
+            audioWin: document.querySelector('.sound__win')
+        },
         gunmanTimeCount,
         date,
         playerDate,
@@ -39,7 +44,7 @@ function Gunman() {
 
     //Timers
     self.gunManOnPosition = function () {
-        //AudioPlay(audio_win);
+        audioPlay(Audio.audioIntro);
         var timerId;
         console.log(gunman.style.backgroundPositionX);
         gunman.classList.toggle('active');
@@ -51,20 +56,20 @@ function Gunman() {
             gunman.style.right = pathGunmanCounter + 'px';
             playerTime.innerHTML = '';
 
-            switch (bcgGunmanCounter) {
+            switch (bgGunmanCounter) {
                 case 0:
-                    gunman.style.backgroundPositionX = bcgGunmanCounter + 'px';
-                    bcgGunmanCounter -= 97;
+                    gunman.style.backgroundPositionX = bgGunmanCounter + 'px';
+                    bgGunmanCounter -= 97;
                     break;
 
                 case -97:
-                    gunman.style.backgroundPositionX = bcgGunmanCounter + 'px';
-                    bcgGunmanCounter -= 97;
+                    gunman.style.backgroundPositionX = bgGunmanCounter + 'px';
+                    bgGunmanCounter -= 97;
                     break;
 
                 case -194:
-                    gunman.style.backgroundPositionX = bcgGunmanCounter + 'px';
-                    bcgGunmanCounter += 194;
+                    gunman.style.backgroundPositionX = bgGunmanCounter + 'px';
+                    bgGunmanCounter += 194;
                     break;
             }
         }, 200);
@@ -72,90 +77,90 @@ function Gunman() {
         setTimeout(function () {
             clearInterval(timerId);
             gunman.style.backgroundPositionX = bgPosX + "px";
-            Fire();
+            firePlay();
             globalCounter++;
-            AudioStop(audio_intro);
+            audioStop(Audio.audioIntro);
         }, 5000);
     };
 
-    function AudioPlay(audio) {
-        audio.autoplay = true;
-        audio.loop = true;
+    function audioPlay(audio) {
+        audio.play();
     }
 
-    function AudioStop(audio) {
-        audio.autoplay = false;
-        audio.loop = false;
+    function audioStop(audio) {
+        audio.pause();
+        audio.currentTime = 0;
     }
-
-    //function AudioPlayOnce (audio) {
-    //    AudioPlay(audio);
-    //    setTimeout(AudioStop(audio), 500);
-    //}
 
     //Fire gameplay
-    function Fire() {
+    function firePlay() {
         gunmanTimeCount = gunmansArr[globalCounter].timer;
-        ready.classList.toggle('active');
+        Bubbles.ready.classList.toggle('active');
         gunmanTime.innerHTML = gunmanTimeCount + '';
-        AudioPlay(audio_win);
         setTimeout(function () {
-            ready.classList.toggle('active');
-            fire.classList.toggle('active');
+            audioPlay(Audio.audioWait)
+        }, 1200);
+        setTimeout(function () {
+            audioPlay(Audio.audioFire);
+        }, 2600);
+        setTimeout(function () {
+            Bubbles.ready.classList.toggle('active');
+            Bubbles.fire.classList.toggle('active');
+            //
             playerTimeCounter();
-            gunman.addEventListener('click', ShootGamePlay);
+            gunman.addEventListener('click', shootGamePlay);
             timerIdLoose1 = setTimeout(function () {
-                gunman.removeEventListener('click', ShootGamePlay);
-                LooseRender();
-                gunman.addEventListener('click', LooseShootTime);
+                gunman.removeEventListener('click', shootGamePlay);
+                looseRender();
+                gunman.addEventListener('click', looseShootTime);
                 timerIdLoose2 = setTimeout(function () {
-                    gunman.removeEventListener('click', LooseShootTime);
-                    Loose();
+                    gunman.removeEventListener('click', looseShootTime);
+                    looseGamePlay();
                     playerTime.innerHTML = 'infinity!'
-                }, 3000)
+                }, 2000)
             }, gunmansArr[globalCounter].timer * 1000);
         }, 3000)
     }
 
     function playerTimeCounter() {
-        date = +new Date();
+        date = new Date();
     }
 
-    function ShootGamePlay() {
-
+    function shootGamePlay() {
+        audioPlay(Audio.audioShot);
         clearTimeout(timerIdLoose1);
         clearTimeout(timerIdLoose2);
-        playerDate = +new Date();
+        playerDate = new Date();
         playerTimeCount = (playerDate - date) / 1000;
         playerTime.innerHTML = playerTimeCount.toFixed(2) + '';
-        fire.classList.toggle('active');
-        gunman.removeEventListener('click', ShootGamePlay);
+        Bubbles.fire.classList.toggle('active');
+        gunman.removeEventListener('click', shootGamePlay);
 
         if (playerTimeCount < gunmanTimeCount) {
-            WinRender();
+            winRender();
             setTimeout(function () {
-                Win();
+                winGamePlay();
             }, 1500);
         } else {
-            LooseRender();
+            looseRender();
             setTimeout(function () {
-                Loose();
+                looseGamePlay();
             }, 1500);
         }
     }
 
-    function LooseShootTime() {
+    function looseShootTime() {
         clearTimeout(timerIdLoose2);
         playerDate = +new Date();
         playerTimeCount = (playerDate - date) / 1000;
         playerTime.innerHTML = playerTimeCount.toFixed(2) + '';
-        fire.classList.toggle('active');
-        gunman.removeEventListener('click', LooseShootTime);
-        Loose();
+        Bubbles.fire.classList.toggle('active');
+        gunman.removeEventListener('click', looseShootTime);
+        looseGamePlay();
     }
 
-    function Win() {
-        win.classList.toggle('active');
+    function winGamePlay() {
+        Bubbles.win.classList.toggle('active');
         rewardSum += gunmansArr[globalCounter].reward;
         reward.innerHTML = rewardSum + '';
         console.log('win');
@@ -164,27 +169,25 @@ function Gunman() {
         }, 120);
         setTimeout(function () {
             clearInterval(timerIdWin);
-            if (globalCounter === 5) {
+            if (globalCounter === MAX_PLAY) {
                 for (var i = 0; i < 6; i++) {
                     winBg.classList.toggle('active');
                 }
-                console.log(rewardSum);
-                console.log(gunman.style.backgroundPositionX);
-                console.log(gunman.style.backgroundPositionY);
+                audioPlay(Audio.audioWin);
                 return;
             }
             pathGunmanCounter = -100;
             setTimeout(function () {
-                win.classList.toggle('active');
+                Bubbles.win.classList.toggle('active');
                 gunman.classList.toggle('active');
                 self.gunManOnPosition();
             }, 2500);
         }, 1000);
     }
 
-    function Loose() {
+    function looseGamePlay() {
         var timerIdLoose = setInterval(function () {
-            loose.classList.toggle('active')
+            gameOver.classList.toggle('active')
         }, 280);
         setTimeout(function () {
             clearInterval(timerIdLoose);
@@ -192,7 +195,8 @@ function Gunman() {
         console.log('loose');
     }
 
-    function WinRender() {
+    function winRender() {
+        audioPlay(Audio.audioShot_Fall);
         bgPosX = -676;
         var posStep = -97;
         var timerId = setInterval(function () {
@@ -208,7 +212,8 @@ function Gunman() {
         }, 4000);
     }
 
-    function LooseRender() {
+    function looseRender() {
+        audioPlay(Audio.audioDeath);
         bgPosX = -289;
         var posStep = -97;
         var timerId = setInterval(function () {
