974abb2b05dcd7993c6327483f3c86e645f99e0b
garbo-a <garbovskij.a@gmail.com>
2016-03-14 18:39:30 +0200

    should be deleted later

---
+++ TS_test/enentHandler-compiled-compiled.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function addEvent(object, type, callback) {
+    if (object == null || typeof object == 'undefined') {
+        return;
+    }
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on" + type] = callback;
+    }
+};
+
+//# sourceMappingURL=enentHandler-compiled.js.map
+
+//# sourceMappingURL=enentHandler-compiled-compiled.js.map
